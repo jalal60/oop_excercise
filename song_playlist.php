fe0a434bd483c225541b365d2a474e7830f52c2e
Md. Shahjalal <jalal2k4@gmail.com>
2023-10-13 11:58:32 +0600

    Update song_playlist.php

--- song_playlist.php
+++ song_playlist.php
@@ -3,20 +3,32 @@ class Song
 {
     public $songTitle;
     public $songId;
+    public $songDuration;
+    public function __construct($songTitle, $songId, $songDuration)
+    {
+        $this->songTitle = $songTitle;
+        $this->songId = $songId;
+        $this->songDuration = $songDuration;
+    }
 }
-$numb = new Song;
-$numb->songTitle = "Numb Song";
-$numb->songId = 1;
-$babySong = new Song;
-$babySong->songId = 2;
-$babySong->songTitle = "Baby Song";
-//print_r($numb);
+$numbSong = new Song("Numb Song", 1, "2.30");
+$babySong = new Song("Baby Song", 2, "3.30");
+$rockSong = new Song("Rock Song", 3, "4.00");
+
 
 class PlayList
 {
-    public $name;
-    public $songs = array();
-
+    private $name;
+    public $songs;
+    public function __construct($name)
+    {
+        $this->name = $name;
+        $this->songs = array();
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
     public function addSong($song)
     {
         $this->songs[] = $song;
@@ -24,16 +36,17 @@ class PlayList
     public function showPlaylist()
     {
         echo $this->name . PHP_EOL;
+
         foreach ($this->songs as $item) {
+            echo "----------------" . PHP_EOL;
             foreach ($item as $key => $value) {
                 echo "{$key}:{$value}\n";
             }
         }
     }
 }
-$rockPlaylist = new PlayList;
-$rockPlaylist->name = "Rock List";
-$rockPlaylist->addSong($numb);
+$rockPlaylist = new PlayList("Rock List");
+$rockPlaylist->addSong($numbSong);
 $rockPlaylist->addSong($babySong);
+$rockPlaylist->addSong($rockSong);
 $rockPlaylist->showPlaylist();
-//print_r($rockPlaylist);
